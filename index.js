0ce7a67b42af93a906d4d61309e4adb64c5918ed
BuildTools <unconfigured@null.spigotmc.org>
2019-06-08 23:38:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express= require('express');
+const app= express();
+
+
+const fs = require('fs');
+var bodyParser = require('body-parser')
+
+
+app.set('view engine', 'pug');
+var urlencodedParser = app.use(bodyParser.urlencoded({ extended: false }));
+var jsonParser = app.use(bodyParser.json());
+
+var userName = "";
+
+//Logowanie
+app.all('/main', (request, response) =>{
+    response.render('main/index', { 'nazwa':request.query.nazwa});
+});
+
+app.get('/notatki', (request, response) =>{
+    if(request.query.nazwa != ""){
+        userName = request.query.nazwa;
+    }
+    response.render('notatki/index', { 'nazwa':userName} );
+});
+
+//Główna strona dodawaniem notatki
+app.post('/notatki', (request, response) =>{
+    var today = new Date();
+
+    filename = "files/" + userName + '.json';
+
+    if (fs.existsSync(filename)) {
+        fs.readFile(filename, function(err, data) {
+            obj = JSON.parse(data); //now it an object
+            obj.table.push({data: today, tekst:request.body.tekst});
+            json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile(filename, json, function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+              }); 
+        });
+    } else {
+        var obj = {
+            table: []
+            };
+
+        obj.table.push({data: today, tekst:request.body.tekst});
+        var json = JSON.stringify(obj);
+        fs.appendFile(filename, json, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        }); 
+    }
+    
+    response.render('notatki/index', { 'tekst':request.body.tekst, 'nazwa':userName, 'data':today} );
+});
+
+//Wyświetlanie notatek
+app.get('/notatki/pokaz', (request, response) =>{
+    console.log('pokaz notatki');
+    filename = "files/" + userName + '.json';
+    console.log('filename: ' + filename);
+    fs.readFile(filename, function(err, data) {
+        obj = JSON.parse(data); //now it an object
+        response.render('notatki/index', { 'notatki':obj, 'nazwa':userName} );
+    });
+});
+
+
+app.get('/book', (request, response) =>{
+    response.render('books/index', { 'nazwa':request.query.nazwa});
+});
+    
+app.get('/book/:id', (request, response) =>{
+    response.send(`Sending book with id ${request.params.id}`)
+});
+
+app.post('/book', (request, response) =>{
+    response.send('New book created.')
+});
+
+app.listen(3000, () =>{
+    console.log('Server running at http://localhost:3000/');
+});
