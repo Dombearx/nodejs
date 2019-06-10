e1de3cf83b510b76e6f07b6c7efc26c3db8251e7
Dombearx <domin.dober@gmail.com>
2019-06-10 09:56:58 +0200

    dziala

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express= require('express');
-const app= express();
+const express = require('express');
+const app = express();
 
 
 const fs = require('fs');
@@ -13,73 +13,75 @@ var jsonParser = app.use(bodyParser.json());
 var userName = "";
 
 //Logowanie
-app.all('/main', (request, response) =>{
-    response.render('main/index', { 'nazwa':request.query.nazwa});
+app.all('/', (request, response) => {
+    response.render('main/index', { 'nazwa': request.query.nazwa });
 });
 
-app.get('/notatki', (request, response) =>{
-    if(request.query.nazwa != ""){
+app.get('/notatki', (request, response) => {
+    if (request.query.nazwa !== "" && userName === "") {
         userName = request.query.nazwa;
     }
-    response.render('notatki/index', { 'nazwa':userName} );
+    response.render('notatki/index', { 'nazwa': userName });
 });
 
 //Główna strona dodawaniem notatki
-app.post('/notatki', (request, response) =>{
+app.post('/notatki', (request, response) => {
     var today = new Date();
+    console.log(userName)
 
     filename = "files/" + userName + '.json';
 
     if (fs.existsSync(filename)) {
-        fs.readFile(filename, function(err, data) {
+        fs.readFile(filename, function (err, data) {
             obj = JSON.parse(data); //now it an object
-            obj.table.push({data: today, tekst:request.body.tekst});
+            obj.notatki.push({ data: today, tekst: request.body.tekst });
             json = JSON.stringify(obj); //convert it back to json
             fs.writeFile(filename, json, function (err) {
                 if (err) throw err;
                 console.log('Saved!');
-              }); 
+            });
         });
     } else {
         var obj = {
-            table: []
-            };
+            notatki: []
+        };
 
-        obj.table.push({data: today, tekst:request.body.tekst});
+        obj.notatki.push({ data: today, tekst: request.body.tekst });
         var json = JSON.stringify(obj);
         fs.appendFile(filename, json, function (err) {
             if (err) throw err;
             console.log('Saved!');
-        }); 
+        });
     }
-    
-    response.render('notatki/index', { 'tekst':request.body.tekst, 'nazwa':userName, 'data':today} );
+
+    response.render('notatki/index', { 'tekst': request.body.tekst, 'nazwa': userName, 'data': today });
 });
 
 //Wyświetlanie notatek
-app.get('/notatki/pokaz', (request, response) =>{
+app.get('/notatki/pokaz', (request, response) => {
     console.log('pokaz notatki');
+    console.log(userName)
     filename = "files/" + userName + '.json';
     console.log('filename: ' + filename);
-    fs.readFile(filename, function(err, data) {
+    fs.readFile(filename, function (err, data) {
         obj = JSON.parse(data); //now it an object
-        response.render('notatki/index', { 'notatki':obj, 'nazwa':userName} );
+        response.render('notatki/index', { 'notatki': obj.notatki, 'nazwa': userName });
     });
 });
 
 
-app.get('/book', (request, response) =>{
-    response.render('books/index', { 'nazwa':request.query.nazwa});
+app.get('/book', (request, response) => {
+    response.render('books/index', { 'nazwa': request.query.nazwa });
 });
-    
-app.get('/book/:id', (request, response) =>{
+
+app.get('/book/:id', (request, response) => {
     response.send(`Sending book with id ${request.params.id}`)
 });
 
-app.post('/book', (request, response) =>{
+app.post('/book', (request, response) => {
     response.send('New book created.')
 });
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
